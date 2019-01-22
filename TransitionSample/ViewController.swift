e8366fb145aa312b549c437974531e64665043fb
yoshimi-shingai <yoshimi.shingai@linecorp.com>
2019-01-22 11:52:53 +0900

    Initial Commit

---
+++ TransitionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionSample
+//
+//  Created by SHINGAI YOSHIMI on 2019/01/22.
+//  Copyright © 2019 SHINGAI YOSHIMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
